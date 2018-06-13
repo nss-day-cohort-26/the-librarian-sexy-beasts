78f39ce1c9436f7be1bef08cdd089e5e4f490b08
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-06-13 11:57:20 -0500

    added adelaide profile

--- customers.js
+++ customers.js
@@ -1,11 +1,9 @@
-const custBD = Object.create({}, {
-  favoriteGenre: " ",
-  emptyBookshelf: [ ],
-  firstName: " ",
-  lastName: " ",
-  adress: " ",
-  libraryCardNum: " ",
-  checkoutBook: 
-  returnBook:  
-
+//in customers.js file add favgenre, first and last name and address to new variable with your name like example below
+let adelaide = Object.create({custDB}, {
 })
+adelaide.favoriteGenre = 'fiction'
+adelaide.firstName = 'Adelaide'
+adelaide.lastName = 'Yoder'
+adelaide.address = '456 avenue'
+
+console.log('adelaide', adelaide);
