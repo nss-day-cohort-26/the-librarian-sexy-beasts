cde1ce89f0568ab0e74c77d50469716bd44dc785
mdroberts74 <mikedroberts88@gmail.com>
2018-06-13 11:55:24 -0500

    I added to the customers.js file. I added to my object..

--- customers.js
+++ customers.js
@@ -1,11 +1,9 @@
-const custBD = Object.create({}, {
-  favoriteGenre: " ",
-  emptyBookshelf: [ ],
-  firstName: " ",
-  lastName: " ",
-  adress: " ",
-  libraryCardNum: " ",
-  checkoutBook: 
-  returnBook:  
-
-})
+//in customers.js file add favgenre, first and last name and address to new variable with your name like example below
+let michael = Object.create({
+    custDB
+}, {})
+michael.favoriteGenre = 'fiction'
+michael.firstName = 'Michael',
+    michael.lastName = "Roberts",
+    michael.adress = " 265 Karnes Dr. ",
+    console.log('michael', michael);
